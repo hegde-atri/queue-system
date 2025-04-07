f15c7bc4d03d455df1b7bad7900d4c74794cd1b1
hegde-atri <iamatrihegde@outlook.com>
2025-04-07 11:18:56 +0100

    fix: navbar refresh issue

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Authenticated, Unauthenticated } from "convex/react";
-import { SignInButton } from "./auth/sign-in";
 import { AuthMenu } from "./auth/auth-menu";
 import { Button } from "./ui/button";
 import Link from "next/link";
@@ -13,21 +12,10 @@ export default function Navbar() {
 
 	return (
 		<nav className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-between px-4 h-16 items-center">
-			<Authenticated>
-				<Link
-					href="/dashboard"
-					className="text-lg font-bold flex gap-2 items-center"
-				>
-					<ListChecks className="h-6 w-6" />
-					<span>QueueFlow</span>
-				</Link>
-			</Authenticated>
-			<Unauthenticated>
-				<Link href="/" className="text-lg font-bold flex gap-2 items-center">
-					<ListChecks className="h-6 w-6" />
-					<span>QueueFlow</span>
-				</Link>
-			</Unauthenticated>
+			<Link href="/" className="text-lg font-bold flex gap-2 items-center">
+				<ListChecks className="h-6 w-6" />
+				<span>QueueFlow</span>
+			</Link>
 			<div>
 				<Authenticated>
 					<AuthMenu />
