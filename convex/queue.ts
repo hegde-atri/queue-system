79440cfdb8eba4a67b365b9479d32c42f43e0b25
hegde-atri <iamatrihegde@outlook.com>
2025-04-07 11:19:11 +0100

    feat: queue schema and functions

---
+++ convex/queue.ts
@@ -0,0 +1,51 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
+
+export const createQueue = mutation({
+  args: { title: v.string(), description: v.string() },
+	handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      throw new Error("User not authenticated");
+    }
+
+		const queueId = await ctx.db.insert("queues", {
+			title: args.title,
+			description: args.description,
+			owner: userId,
+		});
+	},
+});
+
+export const getQueues = query({
+  args: {},
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      throw new Error("User not authenticated");
+    }
+    return await ctx.db
+      .query("queues")
+      .filter(q => q.eq(q.field("owner"), userId))
+      .order("desc")
+      .collect();
+  }
+})
+
+export const deleteQueue = mutation({
+  args: { queueId: v.id("queues") },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      throw new Error("User not authenticated");
+    }
+
+    const queue = await ctx.db.get(args.queueId);
+    if (!queue || queue.owner !== userId) {
+      throw new Error("Queue not found or not owned by user");
+    }
+
+    await ctx.db.delete(args.queueId);
+  },
+})
--- convex/schema.ts
+++ convex/schema.ts
@@ -1,9 +1,33 @@
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
 import { authTables } from "@convex-dev/auth/server";
+import { v } from "convex/values";
  
 const schema = defineSchema({
   ...authTables,
-  // Your other tables...
+  queues: defineTable({
+    title: v.string(),
+    description: v.string(),
+    owner: v.id("users")
+  }),
+  queueMembers: defineTable({
+    queueId: v.id("queues"),
+    user: v.id("users"),
+    // admin, member
+    role: v.string(),
+    joinedAt: v.string(),
+  }),
+  queueItems: defineTable({
+    queueId: v.id("queues"),
+    name: v.string(),
+    ready: v.boolean(),
+    user: v.id("users"),
+    createdAt: v.string(),
+    updatedAt: v.string(),
+  }),
+  bookmarks: defineTable({
+    queueId: v.id("queues"),
+    user: v.id("users"),
+  }),
 });
  
 export default schema;
