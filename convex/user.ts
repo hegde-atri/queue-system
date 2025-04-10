cd012d33f063df0af063aa1ba42a0beb95f14afd
hegde-atri <iamatrihegde@outlook.com>
2025-04-10 18:49:31 +0100

    feat: convex user

--- convex/user.ts
+++ convex/user.ts
@@ -1,5 +1,6 @@
 import { getAuthUserId } from "@convex-dev/auth/server";
 import { query } from "./_generated/server";
+import { v } from "convex/values";
 
 export const currentUser = query({
   args: {},
@@ -8,3 +9,14 @@ export const currentUser = query({
     return userId !== null ? ctx.db.get(userId) : null;
   },
 });
+
+export const getUserName = query({
+  args: { id: v.id("users") },
+  handler: async (ctx, args) => {
+    const user = await ctx.db.get(args.id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return user.name;
+  },
+})
