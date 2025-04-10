31579b944bcf8b179b79a56a807a0a07c4648782
hegde-atri <iamatrihegde@outlook.com>
2025-04-10 18:49:39 +0100

    feat: convex queueMembers

--- convex/queueMembers.ts
+++ convex/queueMembers.ts
@@ -135,3 +135,39 @@ export const updateMember = mutation({
 		});
 	},
 });
+
+export const joinQueue = mutation({
+	args: { queueId: v.id("queues"), user: v.id("users")  },
+	handler: async (ctx, args) => {
+		const userId = await getAuthUserId(ctx);
+		if (!userId) {
+			throw new Error("User not authenticated");
+		}
+
+		const queue = await ctx.db.get(args.queueId);
+		if (!queue) {
+			throw new Error("Queue not found");
+		}
+
+		const member = await ctx.db
+			.query("queueMembers")
+			.filter((q) =>
+				q.and(
+					q.eq(q.field("queueId"), args.queueId),
+					q.eq(q.field("user"), args.user)
+				)
+			)
+			.first();
+
+		if (member) {
+			throw new Error("Member already exists");
+		}
+
+		await ctx.db.insert("queueMembers", {
+			queueId: args.queueId,
+			user: args.user,
+			role: "member",
+			joinedAt: new Date().toISOString(),
+		});
+	},
+});
